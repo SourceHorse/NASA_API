8558b33e1f07cd73b6bb78ce3b8a09592973a25f
Ross Scarborough <rossscarborough@gmail.com>
2017-09-29 16:13:51 -0400

    begin adding date selection

--- js/index.js
+++ js/index.js
@@ -3,15 +3,20 @@ var weekArray = [];
 for (var i = 0; i < 8; i++) {
   var date = new Date();
   date.setDate(date.getDate()+i);
-  var thisWeek = date.getFullYear() + '-' +
+var thisWeek = date.getFullYear() + '-' +
   ((date.getMonth()+1)<10 ? '0' : '') + (date.getMonth()+1) + '-' +
   ((date.getDate())<10 ? '0' : '') + (date.getDate());
   weekArray.push(thisWeek);
-}
+  }
 var todaysDate = weekArray[0];
 var nextWeek = weekArray[7];
+var desiredDate1 = weekArray[7];
+var desidedDate2 = weekArray[0];
 function getFeed() {
-  $.getJSON('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + nextWeek + '&end_date=' + todaysDate + '&api_key=' + APIKey, function(data) {
+  $.getJSON(makeURL(desiredDate1, desidedDate2), function(data) {
     $('#feed').html('<p>Total number of near Earth objects this week: ' + data.element_count + '</p><p>Total objects today: ' + data.near_earth_objects[weekArray[0]].length);
-  });
+});
 };
+function makeURL(date1, date2) {
+  return ('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + date1 + '&end_date=' + date2 + '&api_key=' + APIKey);
+}
